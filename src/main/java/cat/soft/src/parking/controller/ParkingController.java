55e697a777dfd373c58580e0562be33446bf6112
marinesnow34 <m4rinesnow34@gmail.com>
2023-08-07 17:52:34 +0900

    Chore: 컨트롤러 가이드 코드 작성

---
+++ src/main/java/cat/soft/src/parking/controller/ParkingController.java
@@ -0,0 +1,31 @@
+package cat.soft.src.parking.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+
+import cat.soft.config.BaseResponse;
+import cat.soft.config.BaseResponseStatus;
+import cat.soft.src.parking.model.TestRes;
+
+@RestController
+@RequestMapping("/api")
+public class ParkingController {
+
+	@Autowired
+	private ParkingService parkingService;
+
+	@GetMapping(value = {"/test/{test}", "/test/", "/test"})
+	public BaseResponse<TestRes> testRes(@PathVariable(required = false) String test){
+		if(test == null) {
+			System.out.println("hi");
+			return new BaseResponse<>(BaseResponseStatus.DATABASE_ERROR);
+		}
+		TestRes testRes = parkingService.testText(test);
+		return new BaseResponse<>(testRes);
+	}
+}
