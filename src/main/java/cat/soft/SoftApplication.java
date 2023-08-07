3147e957f98ba4b19c72737765c5954a84b74f76
marinesnow34 <m4rinesnow34@gmail.com>
2023-08-07 16:47:35 +0900

    Chore: 초기 세팅

---
+++ src/main/java/cat/soft/SoftApplication.java
@@ -0,0 +1,13 @@
+package cat.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/soft/SoftApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.soft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
