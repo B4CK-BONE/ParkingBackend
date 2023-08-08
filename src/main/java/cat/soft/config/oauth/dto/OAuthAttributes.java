bf24b234d7d6b8d1d04545d450c381b597ce97e8
scott4935 <scott4935@gmail.com>
2023-08-08 17:34:25 +0900

    Feat: "OAuth2User return 형태에 의한 변환 클래스"
    OAuth2User 의 map return을 받아서 provider, scope에 따라 뽑아냄

---
+++ src/main/java/cat/soft/config/oauth/dto/OAuthAttributes.java
@@ -0,0 +1,41 @@
+package cat.soft.config.oauth.dto;
+
+import cat.soft.domain.Users;
+import cat.soft.domain.eums.Role;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@Builder
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String provider;
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .provider("Google")
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public Users toEntity() {
+        return Users.builder()
+                .name(name)
+                .email(email)
+                .provider(provider)
+                .role(Role.USER)
+                .build();
+    }
+}
