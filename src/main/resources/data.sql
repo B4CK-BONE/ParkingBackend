6d12bd515e5ef9cb035a14244b3d6b10d1122926
marinesnow34 <m4rinesnow34@gmail.com>
2023-08-08 18:44:29 +0900

    Chore: 실행시 sql 자동 입력

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO cat.user (address, car, email, phone, role, room_idx)
+VALUES ('101', '12가1234', 'slknalsdknalkn@naver.com', '01012341234', 1, 1);
+INSERT INTO cat.user (address, car, email, phone, role, room_idx)
+VALUES ('102', '31다4245', 'qezxc@naver.com', '01011111111', DEFAULT, 1);
+INSERT INTO cat.user (address, car, email, phone, role, room_idx)
+VALUES ('103', '15다1345', 'aqerdfc@naver.com', '01021111111', DEFAULT, DEFAULT);
+INSERT INTO cat.user (address, car, email, phone, role, room_idx)
+VALUES ('104', '11다1345', 'aqe123rdfc@naver.com', '01021111211', DEFAULT, DEFAULT);
+
+INSERT INTO cat.room (admin_idx)
+VALUES (1);
