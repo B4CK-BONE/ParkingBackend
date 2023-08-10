2a30b4e7aa0c5b09df6c97e737d4f7004e4efe98
smdmim <smdmim@gmail.com>
2023-08-10 18:32:44 +0900

    Feat. DB 계정정보 암호화
    KEY - TEST_KEY
    ID - 5dTcNza9U1HgMa1NcdIdVw==
    비번 - zc7STXf9u2+6gUfCxQKbkw==
    url - KtXW4iimfJHlvl7foazqqL1kuObZuaUIHUwNxVBn6+Jf/BWK/6m1ZQ==

--- src/test/java/cat/soft/SoftApplicationTests.java
+++ src/test/java/cat/soft/SoftApplicationTests.java
@@ -1,5 +1,6 @@
 package cat.soft;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class SoftApplicationTests {
 
 	@Test
 	void contextLoads() {
+		StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+		standardPBEStringEncryptor.setAlgorithm("PBEWithMD5AndDES");
+		standardPBEStringEncryptor.setPassword("TEST_KEY");
+		String encodedPass = standardPBEStringEncryptor.encrypt("jdbc:mysql://localhost:3306/cat");
+		System.out.println("Encrypted Password for admin is : "+encodedPass);
 	}
 
 }
