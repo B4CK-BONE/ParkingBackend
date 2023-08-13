abe0f0db71f0b59a52c45b1c4cca7e4a839c3272
scott4935 <scott4935@gmail.com>
2023-08-13 17:58:47 +0900

    Feat: token 재발급 로직 추가

--- src/main/java/cat/soft/oauth/user/UsersController.java
+++ src/main/java/cat/soft/oauth/user/UsersController.java
@@ -1,9 +1,13 @@
 package cat.soft.oauth.user;
 
+import cat.soft.oauth.auth.dto.PostSigninRes;
 import cat.soft.oauth.auth.dto.PostUserAuthRes;
+import cat.soft.oauth.auth.dto.RefreshTokenRes;
 import cat.soft.oauth.auth.jwt.JwtTokenProvider;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import cat.soft.oauth.user.dto.GetUserRes;
@@ -13,17 +17,20 @@ import cat.soft.oauth.util.BaseResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
+import static cat.soft.oauth.util.BaseResponseStatus.*;
+
 @Slf4j
 @RestController
 @RequestMapping("/api/users")
 public class UsersController {
 	private final JwtTokenProvider jwtTokenProvider;
 	private final UserProvider userProvider;
-
+	private final UserDao userDao;
 	@Autowired
-	public UsersController(JwtTokenProvider jwtTokenProvider, UserProvider userProvider) {
+	public UsersController(JwtTokenProvider jwtTokenProvider, UserProvider userProvider, UserDao userDao) {
 		this.jwtTokenProvider = jwtTokenProvider;
 		this.userProvider = userProvider;
+		this.userDao = userDao;
 	}
 
 	@GetMapping("")
@@ -41,5 +48,32 @@ public class UsersController {
 		PostUserAuthRes postUserAuthRes = userProvider.UserInfoProvider(String.valueOf(claims.get("email")));
 
 		return new BaseResponse<>(postUserAuthRes);
+	}
+
+	@GetMapping("/refresh")
+	public ResponseEntity<RefreshTokenRes> refRefreshToken(@CookieValue(name = "refreshtoken") String token) throws BaseException{
+		Claims claims = jwtTokenProvider.getJwtContents(token);
+		String email = String.valueOf(claims.get("email"));
+		if (userProvider.checkEmail(email) == 0)
+			throw new BaseException(USERS_EMPTY_USER_EMAIL);
+		try {
+			String beforeRefToken = userDao.tokenByEmail(email);
+			if(beforeRefToken != token)
+				throw new BaseException(INVALID_JWT);
+			else {
+				String accessToken = jwtTokenProvider.createAccessToken(email);
+				String refreshToken = jwtTokenProvider.createRefreshToken(email);
+
+				HttpHeaders headers = new HttpHeaders();
+				headers.set("Set-Cookie","refreshToken="+refreshToken+"; HttpOnly");
+
+				RefreshTokenRes refreshTokenRes = new RefreshTokenRes();
+				refreshTokenRes.setAccessToken(accessToken);
+
+				return ResponseEntity.ok().headers(headers).body(refreshTokenRes);
+			}
+		} catch (Exception e) {
+			throw new BaseException(DATABASE_ERROR);
+		}
 	}
 }
