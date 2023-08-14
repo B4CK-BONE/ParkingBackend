86e5776297d455d0db89f8b2c81f4843d82174f8
marinesnow34 <m4rinesnow34@gmail.com>
2023-08-14 11:24:47 +0900

    Feat: 유저 입력 검증 구현

--- src/main/java/cat/soft/src/parking/model/parking/PostAddTimeReq.java
+++ src/main/java/cat/soft/src/parking/model/parking/PostAddTimeReq.java
@@ -3,6 +3,10 @@ package cat.soft.src.parking.model.parking;
 import java.time.ZonedDateTime;
 
 import cat.soft.src.parking.model.Time;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +17,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PostAddTimeReq {
+	@NotBlank(message = "시간을 입력하세요")
+	@Future
 	private ZonedDateTime time;
+	@NotBlank(message = "주차할 공간을 선택하세요.")
+	@Min(value = 1, message = "주차 공간 위치를 확인하세요.")
+	@Max(value = Integer.MAX_VALUE, message = "주차 공간 위치를 확인하세요.")
 	private Integer slot;
 
 	public Time toEntity() {
--- src/main/java/cat/soft/src/parking/model/parking/PostReportReq.java
+++ src/main/java/cat/soft/src/parking/model/parking/PostReportReq.java
@@ -3,6 +3,9 @@ package cat.soft.src.parking.model.parking;
 import java.time.ZonedDateTime;
 
 import cat.soft.src.parking.model.Report;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +14,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PostReportReq {
+	@NotBlank(message = "신고할 유저를 선택하세요.")
+	@Min(value = 1, message = "신고할 유저 정보를 확인하세요.")
+	@Max(value = Integer.MAX_VALUE, message = "신고할 유저 정보를 확인하세요.")
 	private Integer suspect;
 
 	public Report toEntity() {
