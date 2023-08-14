6d91c45dd7baa0d0b997a6a299869a7541fe6917
scott4935 <scott4935@gmail.com>
2023-08-14 18:08:12 +0900

    Fix: 미사용 코드 주석 처리

--- src/main/java/cat/soft/src/oauth/auth/AuthController.java
+++ src/main/java/cat/soft/src/oauth/auth/AuthController.java
@@ -42,39 +42,39 @@ public class AuthController {
 		this.authService = authService;
 	}
 
-	@PostMapping("/signup")
-	public BaseResponse<String> join(@RequestBody PostSignupReq postUserReq) throws BaseException {
-		User user = new User(postUserReq.getEmail());
-		usersService.createUser(user);
-		return new BaseResponse<>("회원가입에 성공하였습니다.");
-	}
+//	@PostMapping("/signup")
+//	public BaseResponse<String> join(@RequestBody PostSignupReq postUserReq) throws BaseException {
+//		User user = new User(postUserReq.getEmail());
+//		usersService.createUser(user);
+//		return new BaseResponse<>("회원가입에 성공하였습니다.");
+//	}
 
-	@PostMapping("/signin/auto")
-	public BaseResponse<PostSigninAutoRes> login(@RequestBody PostSigninAutoReq postLoginReq) {
-		// chk pw
-		String user_pw = EncryptionUtils.encryptSHA256(postLoginReq.getEmail());
-
-		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-			postLoginReq.getEmail(), user_pw);
-		Authentication authentication = authenticationManager.authenticate(authenticationToken);
-		log.info("유저 인증 . 자동 로그인을 진행합니다.");
-
-		PrincipalDetails userEntity = (PrincipalDetails)authentication.getPrincipal();
-		System.out.println(userEntity);
-
-		String user_email = userEntity.getUser().getEmail();
-		String accessToken = jwtTokenProvider.createAccessToken(user_email);
-		String refreshToken = jwtTokenProvider.createRefreshToken(user_email);
-
-		authService.registerRefreshToken(user_email, refreshToken);
-		return new BaseResponse<>(new PostSigninAutoRes(accessToken, refreshToken));
-	}
-
-	@GetMapping("/oauth2/success")
-	public BaseResponse<PostSigninAutoRes> loginSuccess(@RequestParam("accessToken") String accessToken,
-		@RequestParam("refreshToken") String refreshToken) {
-		PostSigninAutoRes postLoginRes = new PostSigninAutoRes(accessToken, refreshToken);
-		return new BaseResponse<>(postLoginRes);
-	}
+//	@PostMapping("/signin/auto")
+//	public BaseResponse<PostSigninAutoRes> login(@RequestBody PostSigninAutoReq postLoginReq) {
+//		// chk pw
+//		String user_pw = EncryptionUtils.encryptSHA256(postLoginReq.getEmail());
+//
+//		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+//			postLoginReq.getEmail(), user_pw);
+//		Authentication authentication = authenticationManager.authenticate(authenticationToken);
+//		log.info("유저 인증 . 자동 로그인을 진행합니다.");
+//
+//		PrincipalDetails userEntity = (PrincipalDetails)authentication.getPrincipal();
+//		System.out.println(userEntity);
+//
+//		String user_email = userEntity.getUser().getEmail();
+//		String accessToken = jwtTokenProvider.createAccessToken(user_email);
+//		String refreshToken = jwtTokenProvider.createRefreshToken(user_email);
+//
+//		authService.registerRefreshToken(user_email, refreshToken);
+//		return new BaseResponse<>(new PostSigninAutoRes(accessToken, refreshToken));
+//	}
+//
+//	@GetMapping("/oauth2/success")
+//	public BaseResponse<PostSigninAutoRes> loginSuccess(@RequestParam("accessToken") String accessToken,
+//		@RequestParam("refreshToken") String refreshToken) {
+//		PostSigninAutoRes postLoginRes = new PostSigninAutoRes(accessToken, refreshToken);
+//		return new BaseResponse<>(postLoginRes);
+//	}
 
 }
