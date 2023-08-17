9c368fd6b97994b20ee3c00b93bb8affe94fc0e1
scott4935 <scott4935@gmail.com>
2023-08-17 15:19:57 +0900

    Feat: 필터링 추가

--- src/main/java/cat/soft/src/oauth/user/dto/GetSurveyReq.java
+++ src/main/java/cat/soft/src/oauth/user/dto/GetSurveyReq.java
@@ -1,5 +1,7 @@
 package cat.soft.src.oauth.user.dto;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 
@@ -8,8 +10,9 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetSurveyReq {
-    @NotNull(message = "내용을 입력하세요")
+    @NotBlank(message = "내용을 입력하세요")
     private String contents;
+    @Pattern(regexp="^(data:image/(jpg|jpeg|png);base64,(/9j/|iVBORw)).*",message = "이미지만 업로드 가능 합니다")
     private String img;
 
 }
