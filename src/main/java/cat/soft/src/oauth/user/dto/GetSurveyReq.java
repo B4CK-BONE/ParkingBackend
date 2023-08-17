ee7e4e7003ceefd054f0e2765c48168a7bd1d8d0
marinesnow34 <m4rinesnow34@gmail.com>
2023-08-17 16:45:28 +0900

    Fix: 정규식 메모리 터지는 문제 해결

--- src/main/java/cat/soft/src/oauth/user/dto/GetSurveyReq.java
+++ src/main/java/cat/soft/src/oauth/user/dto/GetSurveyReq.java
@@ -1,18 +1,20 @@
 package cat.soft.src.oauth.user.dto;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import lombok.*;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetSurveyReq {
-    @NotBlank(message = "내용을 입력하세요")
-    private String contents;
-    @Pattern(regexp="^(data:image/(jpg|jpeg|png);base64,(/9j/|iVBORw)).*",message = "이미지만 업로드 가능 합니다")
-    private String img;
+	@NotBlank(message = "내용을 입력하세요")
+	private String contents;
+	@Pattern(regexp = "^(data:image/(jpg|jpeg|png);base64,(/9j/|iVBORw))[0-9a-zA-Z/+=]*$", message = "이미지만 업로드 가능 합니다")
+	private String img;
 
 }
